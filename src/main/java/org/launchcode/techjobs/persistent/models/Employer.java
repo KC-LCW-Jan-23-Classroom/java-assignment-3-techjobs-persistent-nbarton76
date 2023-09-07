11d10efa32cf2d08c44d0fc3e7bdbf2b8e76bf91
Nichelle Barton <52866856+nbarton76@users.noreply.github.com>
2023-09-06 22:32:31 -0500

    update employer and skill

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,6 +1,28 @@
 package org.launchcode.techjobs.persistent.models;
 
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
 public class Employer extends AbstractEntity {
 
+    private String location;
+    @Entity
+    public Employer() {
+    }
 
+    public Employer(String location) {
+        this.location = location;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String aLocation) {
+        this.location = aLocation;
+    }
+
+    @Override
+    public String toString() {
+        return location;
+    }
 }
