8d09379752e9822d4b12c7dd944684d7ecc90e67
Nichelle Barton <52866856+nbarton76@users.noreply.github.com>
2023-09-06 22:40:59 -0500

    update employer and skill

---
+++ src/main/java/org/launchcode/techjobs/persistent/models/data/EmployerRepository.java
@@ -0,0 +1,6 @@
+package org.launchcode.techjobs.persistent.models.data;
+
+import org.launchcode.techjobs.persistent.models.Employer;
+@Repository
+public interface EmployerRepository extends CrudRepository<Employer, Integer>{
+}
---
+++ src/main/java/org/launchcode/techjobs/persistent/models/data/SkillRepository.java
@@ -0,0 +1,7 @@
+package org.launchcode.techjobs.persistent.models.data;
+
+import org.launchcode.techjobs.persistent.models.Skill;
+
+@Repository
+public interface SkillRepository extends CrudRepository<Skill, Integer>{
+}
